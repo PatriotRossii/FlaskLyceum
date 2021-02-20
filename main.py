3393d22179389532dd257102174ffd1aff3855d3
PatriotRossii <patriotrossii2019@mail.ru>
2021-02-20 17:02:41 +0500

    Add landscapes carousel

--- main.py
+++ main.py
@@ -306,5 +306,43 @@ def load_photo():
         return template.format(url=url_for('static', filename='css/style.css'), image=image)
 
 
+@app.route('/carousel')
+def landscapes():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Пейзажи Марса</title>
+                  </head>
+                  <body>
+                    <center><h1>Пейзажи Марса</h1></center>
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="1000">
+                          <img class="d-block w-100" src="{url_for('static', filename='img/landscapes/landscape_1.jpg')}" alt="First slide">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="1000">
+                          <img class="d-block w-100" src="{url_for('static', filename='img/landscapes/landscape_2.jpg')}" alt="Second slide">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="1000">
+                          <img class="d-block w-100" src="{url_for('static', filename='img/landscapes/landscape_3.png')}" alt="Third slide">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="1000"
+                          <img class="d-block w-100" src="{url_for('static', filename='img/landscapes/landscape_4.jpg')}" alt="Fourth slide">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="1000">
+                          <img class="d-block w-100" src="{url_for('static', filename='img/landscapes/landscape_5.jpg')}" alt="Fifth slide">
+                        </div>
+                      </div>
+                    </div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
